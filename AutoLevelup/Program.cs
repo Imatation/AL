94a6b92892149130f2d19ccaf07bbd670b117780
Imatation <ludo.nuskin@yahoo.fr>
2014-10-17 15:43:22 +0200

    Update Program.cs

--- AutoLevelup/Program.cs
+++ AutoLevelup/Program.cs
@@ -40,7 +40,7 @@ namespace AutoLevelup
             else if (Player.BaseSkinName == "Akali") abilitySequence = new int[] { 1, 2, 3, 1, 1, 4, 1, 3, 1, 3, 4, 3, 3, 2, 2, 4, 2, 2 };
             else if (Player.BaseSkinName == "Alistar") abilitySequence = new int[] { 3, 1, 2, 3, 3, 4, 3, 1, 3, 1, 4, 1, 1, 2, 2, 4, 2, 2 };
             else if (Player.BaseSkinName == "Amumu") abilitySequence = new int[] { 3, 1, 2, 3, 2, 4, 3, 3, 3, 2, 4, 2, 2, 1, 1, 4, 1, 1 };
-            else if (Player.BaseSkinName == "Anivia") abilitySequence = new int[] { 1, 3, 2, 3, 3, 4, 3, 1, 3, 1, 4, 1, 1, 2, 2, 4, 2, 2 };
+            else if (Player.BaseSkinName == "Anivia") abilitySequence = new int[] { 1, 3, 3, 2, 3, 4, 3, 1, 3, 2, 4, 1, 2, 1, 1, 4, 2, 2 };
             else if (Player.BaseSkinName == "Annie")
             {
                 if (ObjectManager.Player.Masteries.Where(mastery => mastery.Page == MasteryPage.Utility)
